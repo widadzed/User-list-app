9196f0ceafa70ea35ad2a753c20c5f5b83a4aede
zediri widad <widadzediri@gmail.com>
2024-07-14 22:38:14 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserList from './UserListe';
+
+const App = () => {
+  return (
+    <div className="App">
+      <UserList />
+    </div>
+  );
+};
+
+export default App;
+
---
+++ src/UserListe.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const UserList = () => {
+  const [listOfUsers, setListOfUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        setListOfUsers(response.data);
+      })
+      .catch(error => {
+        console.error(' error fetching the user data!', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {listOfUsers.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
